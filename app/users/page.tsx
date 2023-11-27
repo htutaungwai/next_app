9f38d5f90c6fbf8ba524858dd838f3de52317add
Htut Aung Wai <defo19987@gmail.com>
2023-11-27 12:53:53 +0700

    my first next.js finished

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+const UsersPage = async () => {
+  const rest = await fetch("https://jsonplaceholder.typicode.com/users/", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await rest.json();
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UsersPage;
